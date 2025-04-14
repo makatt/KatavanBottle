0a727913c8e11585554844d6edfc8395e9ba4cdf
makatt <loshamaks@gmail.com>
2025-04-14 14:19:59 +0300

    myform.py. Добавлет отладчик

--- KatavanBottle/myform.py
+++ KatavanBottle/myform.py
@@ -1,6 +1,9 @@
-from bottle import post, request, response
+п»їfrom bottle import post, request, response
 import re
 from datetime import datetime
+import pdb
+
+questions = {}
 
 @post('/home', method='post')
 def my_form():
@@ -8,21 +11,27 @@ def my_form():
     question = request.forms.get('QUEST')
     username = request.forms.get('USERNAME') 
     
-    # 7.4. Проверка заполненности полей
+    # 7.4. РџСЂРѕРІРµСЂРєР° Р·Р°РїРѕР»РЅРµРЅРЅРѕСЃС‚Рё РїРѕР»РµР№
     if not all([email, question, username]):
         return "Error: Please fill in all fields"
     
-    # 7.1. Проверка формата email
-    email_pattern = r'^[\w]+@[\w]+\.[\w]$'
+    # 7.1. РџСЂРѕРІРµСЂРєР° С„РѕСЂРјР°С‚Р° email
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if not re.match(email_pattern, email):
         return "Error: Invalid email format"
     
-    # 7.3. Получаем текущую дату
+    ###
+    questions[email] = question
+    pdb.set_trace()
+    
+    # 7.3. РџРѕР»СѓС‡Р°РµРј С‚РµРєСѓС‰СѓСЋ РґР°С‚Сѓ
     current_date = datetime.now().strftime("%Y-%m-%d")
     
-    # 7.2. Формируем ответ с именем пользователя и датой
+    # 7.2. Р¤РѕСЂРјРёСЂСѓРµРј РѕС‚РІРµС‚ СЃ РёРјРµРЅРµРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ Рё РґР°С‚РѕР№
     return f"""
     Thanks, {username}! 
     The answer will be sent to the mail: {email}
     Access Date: {current_date}
     """
+
+    
